a51210eb630b057be4e83e55966a9b8f170536b6
unknown <Admin@NB-KEHQ4-ADBT-7.ke.nationmedia.com>
2025-02-21 17:06:14 +0300

    Added product model and seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_21_140221) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "store_logo"
+    t.string "product_image"
+    t.decimal "price"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# db/seeds.rb
+
+require 'faker'
+
+Product.destroy_all # Clear existing data
+
+10.times do
+  Product.create!(
+    name: Faker::Commerce.department(max: 1, fixed_amount: true) + " " + Faker::Commerce.material, 
+    store_logo: Faker::LoremFlickr.image(size: "100x100", search_terms: ['clothing', 'brand']),
+    product_image: Faker::LoremFlickr.image(size: "300x300", search_terms: ['fashion', 'clothes']),
+    price: Faker::Commerce.price(range: 10.0..100.0, as_string: true),
+    description: Faker::Marketing.buzzwords
+  )
+end
+
+puts "✅ Seeded #{Product.count} clothing products!"
