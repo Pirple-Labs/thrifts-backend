3c50cfdaf780b736e8c20fc36b6ae9910bd0c1bb
Joshua Kyengo <jkyengo900@gmail.com>
2025-03-19 15:44:23 +0300

    Dummy product seeding

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins '*'  # Change '*' to your frontend URL in production
-      resource '*',
-        headers: :any,
-        methods: [:get, :post, :patch, :put, :delete, :options],
-        expose: ['Authorization']
-    end
+  allow do
+    origins '*'  # Allow all origins (replace '*' with your frontend domain in production)
+    resource '*', 
+      headers: :any,
+      methods: [:get, :post, :patch, :put, :delete, :options]
   end
-  
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,12 @@
-# db/seeds.rb
-
 require 'faker'
 
 Product.destroy_all # Clear existing data
 
 10.times do
   Product.create!(
-    name: Faker::Commerce.department(max: 1, fixed_amount: true) + " " + Faker::Commerce.material, 
-    store_logo: Faker::LoremFlickr.image(size: "100x100", search_terms: ['clothing', 'brand']),
-    product_image: Faker::LoremFlickr.image(size: "300x300", search_terms: ['fashion', 'clothes']),
+    name: "#{Faker::Commerce.department(max: 1, fixed_amount: true)} #{Faker::Commerce.material}", 
+    store_logo: "https://picsum.photos/seed/#{rand(1000)}/100/100",
+    product_image: "https://picsum.photos/seed/#{rand(1000)}/300/300",
     price: Faker::Commerce.price(range: 10.0..100.0, as_string: true),
     description: Faker::Marketing.buzzwords
   )
